be36d73bccb513c694872f4649e1d9d464585fae
Darren Yeates <dpyeates@googlemail.com>
2021-04-15 11:41:43 +0100

    Android: revert to use previous
    fix for spurious Quaternion behaviour when levelling

--- android/src/main/java/com/sensorworks/RNAttitude/RNAttitudeModule.java
+++ android/src/main/java/com/sensorworks/RNAttitude/RNAttitudeModule.java
@@ -16,6 +16,8 @@ import android.hardware.SensorManager;
 import android.hardware.SensorEventListener;
 import android.util.Log;
 
+import java.util.Arrays;
+
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.BaseActivityEventListener;
@@ -42,34 +44,26 @@ public class RNAttitudeModule extends ReactContextBaseJavaModule implements Life
   private static final byte ROTATE_NONE = 0;
   private static final byte ROTATE_LEFT = 1;
   private static final byte ROTATE_RIGHT = 2;
-  private static final byte W = 0;
-  private static final byte X = 1;
-  private static final byte Y = 2;
-  private static final byte Z = 3;
 
-  private final ReactApplicationContext mReactContext;
-  private final Sensor mRotationSensor;
-  private final SensorManager mSensorManager;
-  private boolean mInverseReferenceInUse;
-  private int mIntervalMillis;
-  private long mNextSampleTime;
+  private final ReactApplicationContext reactContext;
+  private final Sensor rotationSensor;
+  private final SensorManager sensorManager;
+  private boolean inverseReferenceInUse;
+  private int intervalMillis;
+  private long nextSampleTime;
   private int rotation;
-
-  private final float[] coreQuaternion = new float[4];
-  private final float[] inverseReferenceQuaternion = new float[4];
-  private final float[] baseWorldQuaternion;
-  private float[] worldQuaternion = new float[4];
+  private float[] inverseAngles = new float[3];
+  private final float[] refAngles = new float[3];
 
   public RNAttitudeModule(ReactApplicationContext reactContext) {
     super(reactContext);
-    this.mReactContext = reactContext;
-    this.mReactContext.addLifecycleEventListener(this);
-    mSensorManager = (SensorManager) reactContext.getSystemService(Context.SENSOR_SERVICE);
-    mRotationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-    baseWorldQuaternion = getWorldTransformationQuaternion();
-    mInverseReferenceInUse = false;
-    mIntervalMillis = 200;
-    mNextSampleTime = 0;
+    this.reactContext = reactContext;
+    this.reactContext.addLifecycleEventListener(this);
+    sensorManager = (SensorManager) reactContext.getSystemService(Context.SENSOR_SERVICE);
+    rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+    inverseReferenceInUse = false;
+    intervalMillis = 200;
+    nextSampleTime = 0;
     rotation = ROTATE_NONE;
   }
 
@@ -83,12 +77,12 @@ public class RNAttitudeModule extends ReactContextBaseJavaModule implements Life
 
   @Override
   public void onHostResume() {
-    mSensorManager.registerListener(this, mRotationSensor, mIntervalMillis * 1000);
+    sensorManager.registerListener(this, rotationSensor, intervalMillis * 1000);
   }
 
   @Override
   public void onHostPause() {
-    mSensorManager.unregisterListener(this);
+    sensorManager.unregisterListener(this);
   }
 
   @Override
@@ -102,34 +96,27 @@ public class RNAttitudeModule extends ReactContextBaseJavaModule implements Life
   @ReactMethod
   // Determines if this device is capable of providing attitude updates
   public void isSupported(Promise promise) {
-    promise.resolve(mRotationSensor != null);
+    promise.resolve(rotationSensor != null);
   }
 
   @ReactMethod
   // Applies an inverse to core data and compensates for any non-zero installations.
   // Basically it makes a new 'zero' position for both pitch and roll.
   public void zero() {
-    inverseReferenceQuaternion[W] = worldQuaternion[W];
-    inverseReferenceQuaternion[X] = -worldQuaternion[X];
-    inverseReferenceQuaternion[Y] = -worldQuaternion[Y];
-    inverseReferenceQuaternion[Z] = -worldQuaternion[Z];
-    mInverseReferenceInUse = true;
+    inverseAngles = Arrays.copyOf(refAngles, 3);
+    inverseReferenceInUse = true;
   }
 
   @ReactMethod
   // Resets the inverse quaternion in use and goes back to using the default 'zero' position
   public void reset() {
-    mInverseReferenceInUse = false;
-    inverseReferenceQuaternion[W] = 0;
-    inverseReferenceQuaternion[X] = 0;
-    inverseReferenceQuaternion[Y] = 0;
-    inverseReferenceQuaternion[Z] = 0;
+    inverseReferenceInUse = false;
   }
 
   @ReactMethod
   // Sets the interval between event samples
   public void setInterval(int interval) {
-    mIntervalMillis = interval;
+    intervalMillis = interval;
   }
 
   @ReactMethod
@@ -151,26 +138,22 @@ public class RNAttitudeModule extends ReactContextBaseJavaModule implements Life
         Log.e("ERROR", "Unrecognised rotation passed to react-native-attitude, must be 'none','left' or 'right' only");
         break;
     }
-    // reset any inverse rotation as any device rotation will render it incorrect
-    if (mInverseReferenceInUse) {
-      reset();
-    }
   }
 
   @ReactMethod
   public void startObserving(Promise promise) {
-    if (mRotationSensor == null) {
+    if (rotationSensor == null) {
       promise.reject("-1", "Rotation vector sensor not available; will not provide orientation data.");
       return;
     }
-    mNextSampleTime = 0;
-    mSensorManager.registerListener(this, mRotationSensor, mIntervalMillis * 1000);
-    promise.resolve(mIntervalMillis);
+    nextSampleTime = 0;
+    sensorManager.registerListener(this, rotationSensor, intervalMillis * 1000);
+    promise.resolve(intervalMillis);
   }
 
   @ReactMethod
   public void stopObserving() {
-    mSensorManager.unregisterListener(this);
+    sensorManager.unregisterListener(this);
   }
 
   //------------------------------------------------------------------------------------------------
@@ -178,128 +161,81 @@ public class RNAttitudeModule extends ReactContextBaseJavaModule implements Life
 
   @Override
   public void onSensorChanged(SensorEvent sensorEvent) {
+    float[] rotationMatrix = new float[9];
+    float[] remappedRotationMatrix = new float[9];
+
     // Time to run?
     long currentTime = SystemClock.elapsedRealtime();
-    if (currentTime < mNextSampleTime) {
+    if (currentTime < nextSampleTime) {
       return;
     }
 
-    // Extract the quaternion
-    SensorManager.getQuaternionFromVector(coreQuaternion, sensorEvent.values);
+    SensorManager.getRotationMatrixFromVector(rotationMatrix, getVectorFromSensorEvent(sensorEvent));
 
-    // Adjust the core motion quaternion and heading result for left/right screen orientations
-    float[] screenQuaternion;
     if (rotation == ROTATE_LEFT) {
-      screenQuaternion = quaternionMultiply(coreQuaternion, getScreenTransformationQuaternion(90));
+      SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_Z, SensorManager.AXIS_MINUS_X, remappedRotationMatrix);
     } else if (rotation == ROTATE_RIGHT) {
-      screenQuaternion = quaternionMultiply(coreQuaternion, getScreenTransformationQuaternion(-90));
+      SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_MINUS_Z, SensorManager.AXIS_X, remappedRotationMatrix);
     } else {
-      screenQuaternion = coreQuaternion; // no adjustment needed
+      SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Z, remappedRotationMatrix);
     }
 
-    // Adjust the screen quaternion for our 'real-world' viewport.
-    // This is an adjustment of 90 degrees to the core reference frame.
-    // This moves the reference from sitting flat on the table to a frame where
-    // the user is holding looking 'through' the screen.
-    worldQuaternion = quaternionMultiply(screenQuaternion, baseWorldQuaternion);
+    SensorManager.getOrientation(remappedRotationMatrix, refAngles);
 
-    // Extract heading from world quaternion before we apply any inverse reference offset
-    double heading = computeYawEulerAngleFromQuaternion(worldQuaternion);
-
-    // If we are using a inverse reference offset then apply the required transformation
-    if (mInverseReferenceInUse) {
-      worldQuaternion = quaternionMultiply(inverseReferenceQuaternion, worldQuaternion);
+    float[] eulerAngles;
+    if (inverseReferenceInUse) {
+      eulerAngles = getInvertedAngles(refAngles, inverseAngles);
+    }
+    else {
+      eulerAngles = Arrays.copyOf(refAngles, 3);
     }
 
-    // Extract the roll and pitch euler angles from the world quaternion
-    double[] eulerAngles = computeRollPitchEulerAnglesFromQuaternion(worldQuaternion);
+    // Convert radians to degrees, inverse correction needed for pitch to make 'up' positive
+    eulerAngles[PITCH] = (float)-Math.toDegrees(eulerAngles[PITCH]);
+    eulerAngles[ROLL] = (float)Math.toDegrees(eulerAngles[ROLL]);
+    eulerAngles[YAW] = (float)Math.toDegrees(eulerAngles[YAW]);
 
-    // Send change events to the Javascript side via the React Native bridge
+    // Convert -180<-->180 yaw values to 0-->360
+    eulerAngles[YAW] = eulerAngles[YAW] < 0 ? eulerAngles[YAW] + 360 : eulerAngles[YAW];
+
     WritableMap map = Arguments.createMap();
     map.putDouble("timestamp", sensorEvent.timestamp * NS2MS);
     map.putDouble("roll", eulerAngles[ROLL]);
     map.putDouble("pitch", eulerAngles[PITCH]);
-    map.putDouble("heading", heading);
+    map.putDouble("heading", eulerAngles[YAW]);
     try {
-      mReactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("attitudeUpdate", map);
+      reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("attitudeUpdate", map);
     } catch (RuntimeException e) {
       Log.e("ERROR", "Error sending event over the React bridge");
     }
 
     // Calculate the next time we should run
-    mNextSampleTime = currentTime + mIntervalMillis;
+    nextSampleTime = currentTime + intervalMillis;
   }
 
-  private float[] getScreenTransformationQuaternion(float screenOrientation) {
-    float orientationAngle = (float) Math.toRadians(screenOrientation);
-    float minusHalfAngle = -orientationAngle / 2;
-    float[] q_s = new float[4];
-    q_s[W] = (float) Math.cos(minusHalfAngle);
-    q_s[X] = 0;
-    q_s[Y] = 0;
-    q_s[Z] = (float) Math.sin(minusHalfAngle);
-    return q_s;
-  }
-
-  private float[] getWorldTransformationQuaternion() {
-    float worldAngle = (float) Math.toRadians(90.0);
-    float minusHalfAngle = -worldAngle / 2;
-    float[] q_w = new float[4];
-    q_w[W] = (float) Math.cos(minusHalfAngle);
-    q_w[X] = (float) Math.sin(minusHalfAngle);
-    q_w[Y] = 0;
-    q_w[Z] = 0;
-    return q_w;
-  }
-
-  private float[] quaternionMultiply(float[] a, float[] b) {
-    float[] q = new float[4];
-    q[W] = a[W] * b[W] - a[X] * b[X] - a[Y] * b[Y] - a[Z] * b[Z];
-    q[X] = a[X] * b[W] + a[W] * b[X] + a[Y] * b[Z] - a[Z] * b[Y];
-    q[Y] = a[Y] * b[W] + a[W] * b[Y] + a[Z] * b[X] - a[X] * b[Z];
-    q[Z] = a[Z] * b[W] + a[W] * b[Z] + a[X] * b[Y] - a[Y] * b[X];
-    return q;
-  }
-
-  // Calculates the roll and pitch euler angles from quaternion.
-  // See http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToEuler/
-  private double[] computeRollPitchEulerAnglesFromQuaternion(float[] q) {
-    double[] euler = new double[3];
-    double w2 = q[W] * q[W];
-    double x2 = q[X] * q[X];
-    double y2 = q[Y] * q[Y];
-    double z2 = q[Z] * q[Z];
-    double unitLength = w2 + x2 + y2 + z2; // Normalised == 1, otherwise correction divisor.
-    double abcd = q[W] * q[X] + q[Y] * q[Z];
-    double eps = 1e-7;
-    if (abcd > (0.5 - eps) * unitLength) {
-      // singularity at north pole
-      euler[ROLL] = 0.0;
-      euler[PITCH] = Math.PI;
-      euler[YAW] = 0; // calculated independently
-    } else if (abcd < (-0.5 + eps) * unitLength) {
-      // singularity at south pole
-      euler[ROLL] = 0.0;
-      euler[PITCH] = -Math.PI;
-      euler[YAW] = 0; // calculated independently
+  private float[] getVectorFromSensorEvent(SensorEvent event) {
+    if (event.values.length > 4) {
+      // On some Samsung devices SensorManager.getRotationMatrixFromVector
+      // appears to throw an exception if rotation vector has length > 4.
+      // For the purposes of this class the first 4 values of the
+      // rotation vector are sufficient (see crbug.com/335298 for details).
+      // Only affects Android 4.3
+      return Arrays.copyOf(event.values, 4);
     } else {
-      double acbd = q[W] * q[Y] - q[X] * q[Z];
-      euler[ROLL] = Math.toDegrees(Math.atan2(2.0 * acbd, 1.0 - 2.0 * (y2 + x2)));
-      euler[PITCH] = Math.toDegrees(Math.asin(2.0 * abcd / unitLength));
-      euler[YAW] = 0; // calculated independently
+      return event.values;
     }
-    return euler;
   }
 
-  // Calculates the yaw (heading) euler angle only from a quaternion.
-  private double computeYawEulerAngleFromQuaternion(float[] q) {
-    double x2 = q[X] * q[X];
-    double z2 = q[Z] * q[Z];
-    double adbc = q[W] * q[Z] - q[X] * q[Y];
-    double y = Math.toDegrees(-Math.atan2(2.0 * adbc, 1.0 - 2.0 * (z2 + x2)));
-    if (y < 0) {
-      y += 360;
+  private float[] getInvertedAngles(float[] a, float[] b) {
+    a[ROLL] = a[ROLL] - b[ROLL];
+    if (a[ROLL] < -180) {
+      a[ROLL] *= -1;
     }
-    return y;
+    a[PITCH] = a[PITCH] - b[PITCH];
+    if (a[PITCH] < -90) {
+      a[PITCH] *= -1;
+    }
+    return a;
   }
+
 }
